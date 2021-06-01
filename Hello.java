a5eb35999fc04189d6cc361b0a303e538cf6d429
cheenmaya-bhutad <55523581+cheenmaya-bhutad@users.noreply.github.com>
2021-06-01 20:52:06 +0530

    First Java Program Hello World From Netbeans IDE.

---
+++ Hello.java
@@ -0,0 +1,11 @@
+/**
+ * Author - Cheenmaya Bhutad
+ * Date - 06/01/2021
+ * 
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # let-us-java
+This repo is created to store solutions to problems in the book let-us-java by Yashvanth Kanetkar.
+
